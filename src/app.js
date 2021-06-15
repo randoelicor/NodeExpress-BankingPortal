1d09c7c25b29dd8b24b7d6f45e5504dbd5d246f8
randoelicor <Jose_Leonard_Rico@manulife.ca>
2021-06-15 22:09:16 +0800

    File Handling and Routing (Module 2)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const { json } = require('express');
 
 const app = express();
 
@@ -9,8 +10,30 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('./src/public'));
 
+const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8' });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8' });
+const users = JSON.parse(userData);
+
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Index' });
+  res.render('index', { title: 'Account Summary', accounts: accounts });
+});
+
+app.get('/savings', (req, res) => {
+  res.render('account', { account: accounts.savings });
+});
+
+app.get('/checking', (req, res) => {
+  res.render('account', { account: accounts.checking });
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', { user: users[0] });
 });
 
 app.listen(3000, () => console.log('PS Project running on port 3000.'));
