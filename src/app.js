d40dffe0868f6fb95e8f7c55573330ff1f29a038
randoelicor <Jose_Leonard_Rico@manulife.ca>
2021-06-16 12:48:47 +0800

    Added Handling of Fornm Data (Module 3)

--- src/app.js
+++ src/app.js
@@ -5,10 +5,11 @@ const { json } = require('express');
 
 const app = express();
 
-app.set('views', path.join(__dirname, './views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('./src/public'));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8' });
 const accounts = JSON.parse(accountData);
@@ -36,5 +37,35 @@ app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
 });
 
-app.listen(3000, () => console.log('PS Project running on port 3000.'));
+app.get('/transfer', (req, res) => {
+  res.render('transfer');
+});
+
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit });
+});
+
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount);
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
+
+  const accountsJSON = JSON.stringify(accounts);
+
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'utf8');
+
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.post('/payment', (req, res) => {
+  accounts.credit.balance = parseInt(accounts.credit.balance) - parseInt(req.body.amount);
+  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
+
+  const accountsJSON = JSON.stringify(accounts);
+
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'utf8');
+
+  res.render('payment', { message: "Payment Successful", account: accounts.credit });
+});
+
+app.listen(3000, () => console.log('PS Project running on port 3000.', __dirname));
 
